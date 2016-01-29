537b7f3cdf9cbbfc2051336dc3b7a56d212d6dcb
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-29 11:55:58 -0800

    Cleanup notes

--- js/scripts.js
+++ js/scripts.js
@@ -19,40 +19,34 @@ Pizza.prototype.priceCalculator = function(){
   }
 };
 
-
+// User Logic
 $(document).ready(function() {
 
-  // On form submit, set qty and size = user input
+
   $("form#orderForm").submit(function(event) {
     var pizzaSize = $("select#size").val();
     var quantity = $("input#quantity").val();
 
     var newPizza = new Pizza(pizzaSize, quantity);
-    // debugger;
 
-    //Add each checked item with cheese class to the extraCheese array
+
     var extraCheese = $("input.cheese:checkbox:checked").each(function() {
       newPizza.extraCheese.push(extraCheese);
     });
 
-    //Add each checked item with veg class to the vegToppings array
     var vegTopping = $("input.veg:checkbox:checked").each(function() {
       newPizza.vegTopping.push(vegTopping);
     });
 
-    //Add each checked item with meat class to the meatToppings array
     var meatTopping = $("input.meat:checkbox:checked").each(function() {
       newPizza.meatTopping.push(meatTopping);
     });
 
-    //Runs priceCalculator function on the newPizza object
     var pizzaPrice = newPizza.priceCalculator();
 
 
-    //prints price to page
     $("#price").text(pizzaPrice);
 
-    //show results div
     $("#results").show();
 
     event.preventDefault();
